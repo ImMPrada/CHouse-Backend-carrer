c0adc2c60bec9acd99284bb9ab2761ad112ce4c0
migueling20105 <mprada.ing@gmail.com>
2021-02-10 19:32:22 -0800

    Entrega desafio express avanzado 2: intento

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const {setTimeout} = require('timers');
 const colors = require('colors');
-var express = require('express');
-const Archivo =require('./desafio');
-const { notDeepEqual } = require('assert');
+
+const express = require('express');
 const { json } = require('express');
 
-const app=express();
-const archivo = new Archivo.Archivo('Productos.txt');
-const PORT=8080;
-const dir_a= '/api/productos';
-const dir_b='/api/productos/:id';
-const dir_c='/api/productos';
+const Archivo =require('./desafio');
 
-let visitas_a=0;
-let visitas_b=0;
+
+const app=express();
+const PORT=8080;
+
+const archivo = new Archivo.Archivo('Productos.txt');
+
+const dir_='/api'
+const dir_prductos= dir_+'/productos';
+const dir_agregar_producto= dir_+'/producto';
+const dir_eliminar_producto= dir_+'/productoeliminar';
 
 const server=app.listen(PORT, ()=>{
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+    console.log(colors.green.bold(`Servidor http escuchando en el puerto ${server.address().port}`));
     archivo.leer();
 });
-server.on("error",error => console.log(`Se obtuvo un error: ${error}`));
+server.on("error",error => console.log(colors.red.bold(`Se obtuvo un error: ${error}`)));
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
-app.get(dir_a, (req,res)=>{
-    console.log(colors.green.bold(`Request recibido en ${dir_a}`));;
+app.get(dir_,(req,res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get(dir_prductos, (req,res)=>{
+    console.log(colors.green.bold(`Request recibido en ${dir_prductos}`));;
     let datos=archivo.contenido;
     console.log(colors.yellow(datos.items))
     if (Object.entries(datos).length === 0) { 
@@ -34,26 +40,55 @@ app.get(dir_a, (req,res)=>{
     res.send(datos.items);
 });
 
-app.get(dir_b, (req,res)=>{
-    console.log(colors.green.bold(`Request recibido en ${dir_b}`));
-    console.log(colors.green.bold(`The request ${req.params.id}`));
+app.post(dir_agregar_producto, (req,res)=>{
+    console.log(colors.green.bold(`Request recibido en ${dir_agregar_producto}`));
+    console.log(archivo.contenido)
+    console.log(colors.green.bold(req.body));
+    archivo.guardar(req.body);
+    res.send(archivo.contenido.items[archivo.cantidad-1]);
+})
+
+app.post(dir_eliminar_producto, (req,res)=>{
+    console.log(colors.green.bold(`Request recibido en ${dir_eliminar_producto}`));
+    console.log(colors.green.bold(req.body));
     let datos=archivo.contenido;
-    console.log(datos);
     if (Object.entries(datos).length === 0) { 
         res.send(JSON.parse('{"error":"producto no encontrado"}'));
     }
+    for (let i = 0; i < datos.items.length; i++) {
+        if (datos.items[i].id==req.body.id) {
+            datos.items.splice(i,1);
+            archivo.cantidad=archivo.cantidad-1;
+            archivo.contenido.items=datos.items;
+            archivo.borrar();
+        }
+        
+    }
     datos.items.forEach(element => {
-        if (element.id==req.params.id) {
+        if (element.id==req.body.id) {
             console.log(element);
             res.send(element);
         }
     });
 })
 
-app.post(dir_c, (req,res)=>{
-    console.log(colors.green.bold(`Request recibido en ${dir_c}`));
-    console.log(archivo.contenido)
-    console.log(colors.green.bold(req.body));
-    archivo.guardar(req.body);
-    res.send(archivo.contenido.items[archivo.cantidad-1]);
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
